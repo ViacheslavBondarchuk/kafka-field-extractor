09e66b1e6b264c0480e9aa9c403bbb6467979d73
vbondarchuk <viacheslav.bondarchuk.k@gmail.com>
2024-01-29 16:49:36 +0200

    fixes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
+use std::{fs, io};
 use std::collections::HashSet;
+use std::fs::File;
 use std::path::PathBuf;
 
 use clap::Parser;
 use serde::Deserialize;
 
-const CONFIG_FILE_NAME_TEMPLATE: &str = ".kfe.json";\
-
-enum RecordType {
-    Avro,
-    Json,
-}
+const CONFIG_FILE_NAME_TEMPLATE: &str = ".kfe.json";
+const CONFIG_FILE_URL: &str = "https://raw.githubusercontent.com/ViacheslavBondarchuk/kafka-field-extractor/master/.kfe.json";
 
 fn main() {
     let arguments: Arguments = Arguments::parse();
-    load_config(&arguments.brand.unwrap_or("".to_string()));
+    load_config(&arguments.brand);
 }
 
 fn get_config_file_path_buf() -> PathBuf {
@@ -22,9 +20,24 @@ fn get_config_file_path_buf() -> PathBuf {
     config_dir_path_buf
 }
 
-fn load_config(brand: &String) -> () {
+fn download_config(store_path: &PathBuf) {
+    println!("Downloading config file....");
+    let body = reqwest::blocking::get(CONFIG_FILE_URL).expect("Can not download config")
+        .text()
+        .expect("Can not extract body");
+    let mut config_file = File::create(store_path).expect("Can not create config file");
+    io::copy(&mut body.as_bytes(), &mut config_file).expect("Can not store file");
+}
+
+fn load_config(brand: &String) {
     let config_path_buf = get_config_file_path_buf();
-    println!("Loading config for brand: {} by path: {}", brand, config_path_buf.display());
+    if !config_path_buf.exists() {
+        download_config(&config_path_buf);
+    }
+
+    let configs: Vec<Config> = serde_json::from_slice(&fs::read(&config_path_buf).expect("Can not read config"))
+        .expect("Can not deserialize config");
+    println!("Loading config for brand: {} by path: {}", brand, &config_path_buf.display());
 }
 
 
@@ -32,7 +45,13 @@ fn load_config(brand: &String) -> () {
 #[command(author, version)]
 struct Arguments {
     #[arg(short, long)]
-    brand: Option<String>,
+    brand: String,
+}
+
+#[derive(Debug, Deserialize)]
+enum RecordType {
+    Avro,
+    Json,
 }
 
 #[derive(Debug, Deserialize)]
